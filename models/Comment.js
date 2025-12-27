d72609ae25e1e43e0c6885b595f41533c984045f
mosalehkamali <mosalehkamali@gmail.com>
2025-12-27 18:58:37 +0330

    Refactor Comment model for improved structure and functionality: Updated schema to include new fields for parent comments, text, and rating. Switched to ES6 module syntax for better compatibility and added virtual field for replies, enhancing comment relationships.

--- models/Comment.js
+++ models/Comment.js
@@ -1,45 +1,55 @@
-const { default: mongoose } = require("mongoose");
-require("./Product");
-require("./User");
+import mongoose from "mongoose";
 
-const schema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const CommentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+
+    parent: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+      default: null,
+    },
+
+    date: {
+      type: Date,
+      default: () => Date.now(),
+      immutable: false,
+    },
+
+    text: {
+      type: String,
+      required: true,
+    },
+
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+
+    approved: {
+      type: Boolean,
+      default: false,
+    },
   },
-  body: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  score: {
-    type: Number,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: () => Date.now(),
-    immutable: false,
-  },
-  isAccept: {
-    type: Boolean,
-    default: false,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref:"User",
-    required: true,
-  },
-  product: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Product",
-    required: true,
-  },
+  { timestamps: true }
+);
+
+CommentSchema.virtual("replies", {
+  ref: "Comment",
+  localField: "_id",
+  foreignField: "parent",
 });
 
-const model = mongoose.models.Comment || mongoose.model("Comment", schema);
-
-module.exports = model;
+export default mongoose.models.Comment ||
+  mongoose.model("Comment", CommentSchema);
