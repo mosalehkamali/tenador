aed371431a16cab1cc39423a014d0f256015dae4
mosalehkamali <mosalehkamali@gmail.com>
2026-02-03 18:11:58 +0330

    Update import path for global styles in Admin Panel layout: Changed import from site-specific CSS to a more general global CSS file for consistency across the application.

--- src/app/(Admin-Panel)/p-admin/layout.jsx
+++ src/app/(Admin-Panel)/p-admin/layout.jsx
@@ -1,4 +1,4 @@
-import '@/app/(Site)/globals.css';
+import '@/app/globals.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AdminLayout from "@/components/admin/Layout"
